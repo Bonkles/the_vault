07e82fa7636f5f67522a8a3eed7dad5940d5b524
Benjamin Clark <bennyc@gmail.com>
2012-10-18 14:24:21 -0400

    Created a method that searched the DB for ratings, and stores them in an instance variable inside the controller.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,28 +6,48 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
+  def initialize
+    #Look through the db and assemble a list of possible ratings.
+    assemble_ratings
+    #enabled ratings checkboxes! Turn all on by default.
+    @ratings_filter = @all_ratings 
+    super
+  end
+
+#This method checks the database and assembles an instance variable array
+#whose elements consist of the ratings present.   
+  def assemble_ratings
+    @all_ratings = Array.new
+    ratings_movies = Movie.all
+        ratings_movies.each do |movie|
+          unless @all_ratings.include? movie.rating
+            @all_ratings << movie.rating
+      end
+    end
+  end
+  
+  
   def index
     
+    #If the user checked any of the ratings boxes
+    if params.has_key? :ratings
+      @ratings_filter = params[:ratings].keys
+    end
     @title_col_class = nil
     @date_col_class = nil
     @rating_col_class = nil
     #If rating or title are passed, order the elements as such! 
     #Otherwise, display the items as normal
     if params.has_key? :sort_by
+        @sort_column = params[:sort_by]
       if params[:sort_by] == "rating"
-        @sort_column = "rating"
         @rating_col_class = "hilite"
-        return @movies = Movie.order("rating").all
       elsif params[:sort_by] == "title"
-        @sort_column = "title"
         @title_col_class = "hilite"
-        return @movies = Movie.order("title").all
       elsif params[:sort_by] == "release_date"
-        @sort_column = "release_date"
         @date_col_class = "hilite"
-        return @movies = Movie.order("release_date").all
-      else
       end
+        return @movies = Movie.order(params[:sort_by]).all
     end
     return @movies = Movie.all
   end
